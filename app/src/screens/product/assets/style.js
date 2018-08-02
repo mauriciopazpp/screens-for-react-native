3385e97ad6cb5ab9832151484150ad14cbac976e
Mauricio Paz <mauriciopazdesign@gmail.com>
2018-08-02 00:37:29 -0300

    ProductsScreen

---
+++ app/src/screens/product/assets/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+const style = props => StyleSheet.create(props)
+
+export const index = style({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+})
